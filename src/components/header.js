712f077e05233683e532e93508d37f1a17a872b9
ch264 <christinavhastenrath@gmail.com>
2020-03-07 15:06:20 -0800

    added menu to show toggle

--- src/components/header.js
+++ src/components/header.js
@@ -3,10 +3,30 @@ import PropTypes from "prop-types"
 import React from "react"
 import '../style/components/_header.scss'
 
-const Header = ({ siteTitle }) => (
-  <div>
+const Header = ({ siteTitle, toggleNav, setToggleNav }) => {
+  // const [toggleNav, setToggleNav] = React.useState(false);
+  return (
+  <div className={`site-wrapper ${toggleNav ? `header-open` : ``}`}>
     <header className="header">
       <div id="menu" className="header-container">
+        <a
+          className="nav-burger"
+          href={`#menu`}
+          onClick={() => setToggleNav(!toggleNav)}
+        >
+          <div
+            className="hamburger hamburger--collapse"
+            aria-label="Menu"
+            role="button"
+            aria-controls="navigation"
+          >
+            <div className="hamburger-box">
+              <div className="hamburger-inner" />
+            </div> 
+            <div className="hamburger-text-menu-text hidden">Menu</div>
+          </div>
+        </a>
+
         <nav id="swup" className="header-left">
           <ul className="nav" role="menu">
             <li className="nav-home" role="menuitem">
@@ -66,7 +86,7 @@ const Header = ({ siteTitle }) => (
       </div>
     </header>
   </div>
-)
+)};
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
