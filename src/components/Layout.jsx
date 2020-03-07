6d5d65181501a994fd17367e75122635b6b65aec
ch264 <christinavhastenrath@gmail.com>
2020-03-07 15:45:21 -0800

    added footer to layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -8,9 +8,8 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
-import "./layout.css"
+import Header from "./Header"
+import Footer from './Footer'
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -22,25 +21,18 @@ const Layout = ({ children }) => {
       }
     }
   `)
-
+// const Layout = props => {
+//   const { title, children } = props;
+//   console.log(props)
   return (
-    <>
+    <div>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
+      {/* <Header siteTitle={title} /> */}
+      
+      <main>{children}</main>
+      
+      <Footer /> 
+    </div>
   )
 }
 
