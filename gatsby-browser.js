a2a7bfac2fc07798f3152164a40920a9c01c6bd5
ch264 <christinavhastenrath@gmail.com>
2020-03-21 15:44:15 -0700

    adjusted google font and nav lins

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,6 @@
 // typeface
 import "./src/style/_all.scss"
-import "typeface-muli"
+// import "typeface-muli"
 // import "typeface-montserrat"
 // import "typeface-merriweather"
 // import "typeface-nunito"
