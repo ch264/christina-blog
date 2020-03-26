5ed29d20d5b92002e1b9574d575d93c8552da434
ch264 <christinavhastenrath@gmail.com>
2020-03-25 18:05:44 -0700

    removing bug

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -62,35 +62,29 @@ class ContentWithImage extends Component {
 		);
 	}
 }
-
 class ContentNoImage extends Component {
 	render() {
 		const { props } = this.props;
 		return (
 			<div className="post-card-content">
-
 				{/* tags */}
 				<div>
 					<Tags tags={props.node.frontmatter.tags} /> 
 				</div>
-
 			{/* title */}
 				<div>
 					<Link to={props.node.fields.slug} className="post-card-link">
 						<h2 className="post-card-title">{props.node.frontmatter.title || props.node.fields.slug}</h2>
 					</Link>
 				</div>
-
 			{/* date */}
 				<div className="post-card-date">
 					{props.node.frontmatter.date}
 				</div>
-
 			{/* body */}
 				<div className="post-card-body">
 					{props.node.frontmatter.description || props.node.excerpt}
 				</div>
-
 			{/* read more */}
 				<div>
 					<Link to={props.node.fields.slug} className="post-card-link post-card-readmore">
