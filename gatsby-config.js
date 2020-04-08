066f40cbfc6586a911850a56cd526e1de15fe5d4
ch264 <christinavhastenrath@gmail.com>
2020-04-07 17:12:10 -0700

    logofix

--- gatsby-config.js
+++ gatsby-config.js
@@ -103,17 +103,17 @@ module.exports = {
         icon: `src/images/favicon-96x96.png`, // This path is relative to the root of the site.
       },
     },
-    {
-      resolve: `gatsby-plugin-purgecss`,
-      options: {
-        printRejected: true, // Print removed selectors and processed file names
-        develop: true, // Enable while using `gatsby develop`
-        // tailwind: true, // Enable tailwindcss support
-        // whitelist: ['whitelist'], // Don't remove this selector
-        ignore: ['/ignored.css', 'prismjs/', '/prism.css', 'docsearch.js/'], // Ignore files/folders
-        purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-purgecss`,
+    //   options: {
+    //     printRejected: true, // Print removed selectors and processed file names
+    //     develop: true, // Enable while using `gatsby develop`
+    //     // tailwind: true, // Enable tailwindcss support
+    //     // whitelist: ['whitelist'], // Don't remove this selector
+    //     ignore: ['/ignored.css', 'prismjs/', '/prism.css', 'docsearch.js/'], // Ignore files/folders
+    //     purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
+    //   },
+    // },
     'gatsby-plugin-sitemap',
     // {
     //   resolve:  'gatsby-plugin-sitemap',
